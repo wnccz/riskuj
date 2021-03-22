fc823705441dd3b4adbafea67184f1e15dfa57de
Tomáš Brzek <tomas@brzek.net>
2021-03-22 16:26:10 +0100

    Enhance UX and UI of player adding form
    - added TWB classes to name input and add button
    - set focus to name input after adding player
    - clear name input after adding player

--- src/components/playerSettings.vue
+++ src/components/playerSettings.vue
@@ -5,8 +5,10 @@
         </div>
         <div class="card-body">
             <player-item :player="player" :key="index" :id="index" v-for="(player, index) in players"></player-item>
-            <input v-model="inPlayerName" placeholder="Jméno hráče">
-            <button @click="addPlayer">Přidat</button>  
+            <div class="d-flex">
+                <input v-model="inPlayerName" placeholder="Jméno hráče" ref="inPlayerName" class="form-control mb-1 me-1">
+                <button @click="addPlayer" class="btn btn-success mb-1">Přidat</button>
+            </div>
         </div>
     </div>
 </template>
@@ -26,6 +28,8 @@ export default {
     methods: {
         addPlayer: function() {
             this.$store.commit('addPlayer',  this.inPlayerName);
+            this.inPlayerName = '';
+            this.$refs.inPlayerName.focus();
         }
     }
 }
